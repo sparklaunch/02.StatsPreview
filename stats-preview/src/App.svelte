9178356d512f3d95c03a8aa5a0f1e34e5a25ef01
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-14 08:53:41 +0900

    Add the app component

--- stats-preview/src/App.svelte
+++ stats-preview/src/App.svelte
@@ -1,3 +1,22 @@
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lexend+Deca&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
 <div id="app">
   <p>Hello World!!</p>
 </div>
+
+<style>
+  #app {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(13, 14, 26);
+  }
+</style>
